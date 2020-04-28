46a88f74d5242967afb6aa9825774dc2c7b1c4da
Vinicius Gomes <vinigomes.0311@gmail.com>
2020-04-27 22:50:45 -0300

    Version 0

---
+++ hunt/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style = {styles.welcome}>kkk novamente Vini</Text>
+      <View style={styles.box}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box: {
+    width:60,
+    height:60,
+    backgroundColor: "#f00"
+  }
+});
